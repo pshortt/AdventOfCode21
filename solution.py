ce51f12088966c4f8c362cadea67d0f5e90055ff
pat <pshortt21@gmail.com>
2021-12-18 19:49:29 -0500

    abstract class for puzzle solutions

---
+++ solution.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from abc import abstractmethod
+
+@dataclass
+class Solution():
+    '''Template for a solution for an Advent of Code puzzle'''
+    filename: str
+   
+    @abstractmethod
+    def solve_part1(self) -> int:
+        '''
+        Implement solution to part 1
+        '''
+        return
+
+    @abstractmethod
+    def solve_part2(self) -> int:
+        '''
+        Implement solution to part 2
+        '''
+        return
+
+    # Class methods
+    def input(self):
+       with open(self.filename) as f:
+            return [l.strip() for l in f.readlines()]    
